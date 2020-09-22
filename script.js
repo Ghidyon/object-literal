2f298999ef7a27d64119ad767c352908b3a85a28
Ghidyon <gideonakunana@gmail.com>
2020-09-22 23:29:48 +0100

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-const getPercentRaise = (years, performance) => raiseInSalary = (years / 10) * performance;
+const employee = {
+    "alfred": {
+        age: 47,
+        years: 20,
+        performance: 7,
+        salary: 10000
+    },
+
+    "john": {
+        age: 55,
+        years: 26,
+        performance: 10,
+        salary: 14000
+    },
+
+    "joshua": {
+        age: 60,
+        years: 35,
+        performance: 9,
+        salary: 20000
+    },
+
+    "daniel": {
+        age: 30,
+        years: 5,
+        performance: 7,
+        salary: 10500
+    },
+
+    "jamie": {
+        age: 40,
+        years: 14,
+        performance: 6,
+        salary: 13000
+    },
+}
+
+const getPercentRaise = (years, performance) => {
+    let raiseInSalary = (years / 10) * performance;
+    return raiseInSalary;
+}
 
 const getIncreasedSalary = (age, years, performance, salary) => {
     let increasedSalary = ((getPercentRaise(years, performance) / 100) * salary) + salary;
@@ -11,47 +51,40 @@ const getIncreasedSalary = (age, years, performance, salary) => {
     }
 }
 
-const employee = {
-    "alfred": {
-        age: 47,
-        years: 20,
-        performance: 7,
-        salary: 10000,
-        newSalary: getIncreasedSalary(47, 20, 7, 10000)
+const newData = {
+    "percentRaise": {
+        alfred: getPercentRaise(20, 7),
+        john: getPercentRaise(26, 10),
+        joshua: getPercentRaise(35, 9),
+        daniel: getPercentRaise(5, 7),
+        jamie: getPercentRaise(14, 6)
     },
 
-    "john": {
-        age: 55,
-        years: 26,
-        performance: 10,
-        salary: 14000,
-        newSalary: getIncreasedSalary(55, 26, 10, 14000)
-    },
-
-    "joshua": {
-        age: 60,
-        years: 35,
-        performance: 9,
-        salary: 20000,
-        newSalary: getIncreasedSalary(60, 35, 9, 20000)
-    },
-
-    "daniel": {
-        age: 30,
-        years: 5,
-        performance: 7,
-        salary: 10500,
-        newSalary: getIncreasedSalary(30, 5, 7, 10500)
-    },
-
-    "jamie": {
-        age: 40,
-        years: 14,
-        performance: 6,
-        salary: 13000,
-        newSalary: getIncreasedSalary(40, 14, 6, 13000)
+    "salaries": {
+        alfred: getIncreasedSalary(47, 20, 7, 10000),
+        john: getIncreasedSalary(55, 26, 10, 14000),
+        joshua: getIncreasedSalary(60, 35, 9, 20000),
+        daniel: getIncreasedSalary(30, 5, 7, 10500),
+        jamie: getIncreasedSalary(40, 14, 6, 13000)
     }
 }
+
+console.log(employee.salaries)
+
+const get15PercentRaise = () => {
+    const percentList = [];
+    let employeeNames = Object.keys(newData.percentRaise);
+    let percents = Object.values(newData.percentRaise);
+    for (let i = 0; i < percents.length; i++) {
+        if (percents[i] >= 15) {
+            percentList.push(employeeNames[i]);
+        }
+    }
+    return percentList;
+}
+
+console.log(get15PercentRaise())
+
 
 console.log([
     { "alfred": employee.alfred.newSalary },
@@ -61,5 +94,15 @@ console.log([
     { "jamie": employee.jamie.newSalary }
 ]);
 
-//console.log(Object.values(employee.joshua))
+/*
+var person = Object.keys(employee)[0];
+var personSalary = Object.values(employee.alfred)[4];
+var obj = {
+    salaries: [
+        { person: personSalary },
+        { person: personSalary }
+    ]
+}
+
+console.log(obj) */
 
