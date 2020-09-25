8ec823de5c18c772723dc5103352a613b881dbe1
Gideon Akunana <gideonakunana@gmail.com>
2020-09-25 09:10:52 +0100

    fifth commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Object Literal of employees
+// Object literal of employees
 const employee = {
     "alfred": {
         age: 47,
@@ -38,13 +38,13 @@ const employee = {
 
 
 const companyRaiseData = (data) => {
-    //function to determine the percentage raise
+    // Function to determine the percentage raise
     const getPercentRaise = (years, performance) => {
         let raiseInSalary = (years / 10) * performance;
         return raiseInSalary;
     }
 
-    //function to determine the new increased salary
+    // Function to determine the new increased salary
     const getIncreasedSalary = (age, years, performance, salary) => {
         let increasedSalary = ((getPercentRaise(years, performance) / 100) * salary) + salary;
         if (age > 50) {
@@ -56,7 +56,7 @@ const companyRaiseData = (data) => {
         }
     }
 
-    //Object literal of new percentage raise and increased salaries
+    // Object literal of new percentage raise and increased salaries
     const newData = {
         "percentRaise": {
             alfred: getPercentRaise(20, 7),
@@ -75,7 +75,7 @@ const companyRaiseData = (data) => {
         }
     }
 
-    //function to determine which employee has atleast 15% raise
+    // Function to determine which employee has atleast 15% raise
     const get15PercentRaise = () => {
         const percentList = [];
         const employeeNames = Object.keys(newData.percentRaise); //Converts Object properties to an array
@@ -88,21 +88,21 @@ const companyRaiseData = (data) => {
         return percentList;
     }
 
-    //function to calculate the sum of all employees' salaries before increase
+    // Function to calculate the sum of all employees' salaries before increase
     const getSumOfOldSalaries = () => {
         const sumOfOldSalaries = employee.alfred.salary + employee.john.salary +
             employee.joshua.salary + employee.daniel.salary + employee.jamie.salary;
         return sumOfOldSalaries;
     }
 
-    //function to calculate the sum of all employees' salaries after increase
+    // Function to calculate the sum of all employees' salaries after increase
     const getSumOfNewSalaries = () => {
         const arrOfNewSalaries = Object.values(newData.salaries);
         const sumOfNewSalaries = arrOfNewSalaries.reduce((a, b) => a + b);
         return sumOfNewSalaries;
     }
 
-    //function to determine the overall payout increase
+    // Function to determine the overall payout increase
     const getOverallPayoutIncrease = () => getSumOfNewSalaries() - getSumOfOldSalaries();
 
     return {
